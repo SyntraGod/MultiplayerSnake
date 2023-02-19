f1cf54711abe425761c316dd1179ed09d37ba26b
SyntraGod <103126837+SyntraGod@users.noreply.github.com>
2023-02-19 23:29:03 +0700

    Update SQLQuery1.sql

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -6,9 +6,11 @@ create table player(
 	idPlayer int  auto_increment primary key not null,
 	usernamePlayer char(100) unique not null,
 	passwordPlayer char(100) not null,
+	statusPlayer int not null,
 	score int,
     highScore int
 );
+
 drop table player ;
 
 select * from player;
